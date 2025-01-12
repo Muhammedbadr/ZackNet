285107b498d36523d02b46bba88bab692c1b8e1a
Muhammedbedir <mukhammadnur6685@gmail.com>
2025-01-12 21:17:36 +0300

    add Post input

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,6 +3,7 @@ class PostController < ApplicationController
         post = Post.create(user_id: current_user.id, post_text: params[:post_text], has_image: false)
       
         if post.save
+        post.image.attach(params[:image])
           redirect_to request.referrer, notice: "Post added successfully!", allow_other_host: true
         else
           redirect_to request.referrer, alert: "Failed to add the post: #{post.errors.full_messages.to_sentence}", allow_other_host: true
