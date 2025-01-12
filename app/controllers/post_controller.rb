9dfae4cabf4b1c7a9066915126310013a87c0ea6
Muhammedbedir <mukhammadnur6685@gmail.com>
2025-01-12 17:42:11 +0300

    activate create post form

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,12 @@
+class PostController < ApplicationController
+    def create
+        post = Post.create(user_id: current_user.id, post_text: params[:post_text], has_image: false)
+      
+        if post.save
+          redirect_to request.referrer, notice: "Post added successfully!", allow_other_host: true
+        else
+          redirect_to request.referrer, alert: "Failed to add the post: #{post.errors.full_messages.to_sentence}", allow_other_host: true
+        end
+      end
+      
+end
